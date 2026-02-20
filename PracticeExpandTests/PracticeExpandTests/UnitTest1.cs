665ca69f51668e6ff27424ad48840287b1abd02a
Krzysztof Taziu <taziuyo@gmail.com>
2026-02-20 11:27:54 +0100

    First Commit

---
+++ PracticeExpandTests/PracticeExpandTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace PracticeExpandTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
